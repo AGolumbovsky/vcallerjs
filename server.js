ada8cdae0e19a31ffb7bc39290fd75b5d01c9ca3
Andriy <agolumbovsky@gmail.com>
2020-12-13 00:13:07 -0500

    port from 8888 to 8787, a vdroog

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 8888;
+const PORT = process.env.PORT || 8787;
 
 app.use(express.static(__dirname + '/public'));
 
