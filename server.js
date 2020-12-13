5a148fe994e2370b8039042149474d67b3fb0baa
Andriy <agolumbovsky@gmail.com>
2020-12-13 00:10:16 -0500

    oopsie, let is not supported not strict mode

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 8888;
 
 app.use(express.static(__dirname + '/public'));
 
-let currentDate = new Date();
-let timestamp = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+var currentDate = new Date();
+var timestamp = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
 
 app.listen(PORT, () => {
     console.log("vcaller REST API is running, timestamp: " + PORT + "\n" + timestamp);
